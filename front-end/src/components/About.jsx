a5c7d0119ecc356440ab4387210527cc2c91f191
youcef23061983 <youcef.belcourtois@gmail.com>
2025-06-11 15:51:10 +0100

    ignore commit

---
+++ front-end/src/components/About.jsx
@@ -0,0 +1,136 @@
+import Banner from "../pages/Banner";
+import { Link } from "react-router-dom";
+import { useRef } from "react";
+import { useScroll, motion, useTransform } from "framer-motion";
+import UseFetch from "./UseFetch";
+import { ReactLenis } from "lenis/react";
+import { Helmet } from "react-helmet-async";
+
+const About = () => {
+  const url = `${import.meta.env.VITE_PROD_URL_URL}/gallery`;
+
+  const key = "gallery";
+
+  const { data, isPending, error } = UseFetch(url, key);
+
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["0 1", "0.4 1"],
+  });
+  const scrollOpacity = useTransform(scrollYProgress, [0, 0.5, 1], [0, 0.3, 1]);
+  const scrollX = useTransform(scrollYProgress, [0, 1], [-900, 0]);
+  const scrollImg = useTransform(scrollYProgress, [0, 1], [900, 0]);
+  const scrollXP = useTransform(scrollYProgress, [0, 1], [-1800, 0]);
+
+  if (isPending) return <h2>...is loading</h2>;
+  if (error) return <h2>{error.message}</h2>;
+
+  return (
+    <>
+      <Helmet>
+        <title>About Us - LEGEND Hotel</title>
+        <meta
+          name="description"
+          content="Learn more about LEGEND Hotel in Malaysia. Discover our story, mission, and values."
+        />
+        <meta property="og:title" content="About Us - LEGEND Hotel" />
+        <meta
+          property="og:description"
+          content="Learn more about LEGEND Hotel in Malaysia. Discover our story, mission, and values."
+        />
+        <meta property="og:image" content={data && data[0].images[0]} />
+        <meta property="og:url" content={window.location.href} />
+
+        {/* Twitter Meta Tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="About Us - LEGEND Hotel" />
+        <meta
+          name="twitter:description"
+          content="Learn more about LEGEND Hotel in Malaysia. Discover our story, mission, and values."
+        />
+        <meta name="twitter:image" content={data && data[0].images[0]} />
+        <meta name="robots" content="index, follow" />
+        <meta
+          name="keywords"
+          content="about us, hotel, Malaysia, LEGEND Hotel, story, mission, values"
+        />
+        <meta name="author" content="LEGEND Hotel" />
+      </Helmet>
+
+      <ReactLenis
+        root
+        options={{
+          lerp: 0.05,
+        }}
+      >
+        <div
+          className="headerimages"
+          data-testid="about-element"
+          style={{
+            background: `url(${data && data[7].images[0]}) center/cover `,
+          }}
+        >
+          <Banner title="ABOUT US" />
+        </div>
+        <div className="section" ref={ref}>
+          <motion.div
+            className="imgDiv"
+            style={{
+              opacity: scrollOpacity,
+              x: scrollImg,
+              animationDuration: 2,
+            }}
+          >
+            <img
+              src={data && data[7].images[1]}
+              alt="CoastalImg"
+              loading="lazy"
+              className="img"
+            />
+          </motion.div>
+          <div className="p">
+            <motion.h2
+              style={{
+                x: scrollX,
+                opacity: scrollOpacity,
+              }}
+            >
+              Coastal Elegance: Legend Hotel's Enchanting Locale in Batu
+              Ferringhi
+            </motion.h2>
+            <motion.p
+              data-testid="about-paragraph"
+              style={{
+                x: scrollXP,
+                opacity: scrollOpacity,
+              }}
+            >
+              Nestled along the pristine shores of Batu Ferringhi in Malaysia,
+              Legend Hotel unveils a haven of coastal elegance that captures the
+              essence of paradise. Positioned against the backdrop of the
+              Andaman Sea, our hotel offers a breathtaking panorama of azure
+              waters and lush landscapes. The idyllic location provides a
+              perfect blend of serenity and vibrancy, with the lively energy of
+              the nearby markets harmonizing with the tranquil ambiance of our
+              coastal oasis. Guests are invited to explore the captivating
+              surroundings, from the vibrant street art of Batu Ferringhi to the
+              sun-kissed beaches just steps away. Legend Hotel's strategic
+              locale not only provides a picturesque retreat but also serves as
+              a gateway to the rich cultural tapestry and natural wonders that
+              define this enchanting region. Whether you seek relaxation or
+              adventure, our hotel's prime location ensures that every moment is
+              immersed in the beauty and allure of Batu Ferringhi's coastal
+              charm.
+            </motion.p>
+            <Link className="link-btn" to="../google">
+              location
+            </Link>
+          </div>
+        </div>
+      </ReactLenis>
+    </>
+  );
+};
+
+export default About;
---
+++ front-end/src/components/ContactUs.jsx
@@ -0,0 +1,194 @@
+import Banner from "../pages/Banner";
+import { useRef, useEffect, useState } from "react";
+import { useScroll, useTransform, motion } from "framer-motion";
+import { MdAttachEmail } from "react-icons/md";
+import emailjs from "@emailjs/browser";
+import UseFetch from "./UseFetch";
+import { ReactLenis } from "lenis/react";
+const ContactUs = ({ onSubmit }) => {
+  const [formStatus, setFormStatus] = useState(null);
+  const [user, setUser] = useState({ name: "", email: "", comment: "" });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser((prev) => ({ ...prev, [name]: value }));
+  };
+  const url = `${import.meta.env.VITE_PROD_URL_URL}/gallery`;
+  const key = "gallery";
+  const { data, isPending, error } = UseFetch(url, key);
+  const form = useRef();
+  useEffect(() => {
+    document.title = "Contact Us";
+  }, []);
+  const sendEmail = (e) => {
+    e.preventDefault();
+    if (!user.name || !user.email || !user.comment) {
+      alert("Please enter your information");
+      return; // Return early to prevent further execution
+    }
+    if (onSubmit) {
+      onSubmit(user);
+      return; // Skip the emailjs logic in tests
+    }
+
+    emailjs
+      .sendForm("service_vgkozvc", "template_sv5btsr", form.current, {
+        publicKey: import.meta.env.VITE_CONTACT_PUBLIC_KEY,
+      })
+      .then(
+        () => {
+          setFormStatus("Message sent successfully!");
+          setUser({ name: "", email: "", comment: "" });
+          setTimeout(() => {
+            setFormStatus(null);
+          }, 5000);
+        },
+        (error) => {
+          setFormStatus(`Failed to send message: ${error.text}`);
+        }
+      );
+  };
+  const useMediaQuery = (query) => {
+    const [matches, setMatches] = useState(false);
+
+    useEffect(() => {
+      const media = window.matchMedia(query);
+      if (media.matches !== matches) {
+        setMatches(media.matches);
+      }
+
+      const listener = () => {
+        setMatches(media.matches);
+      };
+
+      if (typeof media.addEventListener === "function") {
+        media.addEventListener("change", listener);
+      } else {
+        media.addListener(listener);
+      }
+
+      return () => {
+        if (typeof media.removeEventListener === "function") {
+          media.removeEventListener("change", listener);
+        } else {
+          media.removeListener(listener);
+        }
+      };
+    }, [matches, query]);
+
+    return matches;
+  };
+  const isMediumScreen = useMediaQuery("(min-width: 768px)");
+
+  const ref1 = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref1,
+    offset: ["0 1", isMediumScreen ? "0.6 1" : "0.4 1"],
+  });
+  const scrollOpacity = useTransform(
+    scrollYProgress,
+    [0, 0.5, 0.7, 1],
+    [0, 0.1, 0.3, 1]
+  );
+  const scrollSend = useTransform(scrollYProgress, [0, 1], ["100vw", "0vw"]);
+  const scrollITouch = useTransform(scrollYProgress, [0, 1], ["-100vw", "0vw"]);
+
+  if (isPending) return <h2>...is loading</h2>;
+  if (error) return <h2>{error.message}</h2>;
+
+  return (
+    <div>
+      <div
+        data-testid="custom-element"
+        className="headerimages"
+        style={{
+          background: `url(${data && data[0].images[0]}) center/cover `,
+        }}
+      >
+        <Banner title="CONTACT US">
+          <div className="iconDetail">
+            <MdAttachEmail className="icon" />
+            <p>Email</p>
+          </div>
+        </Banner>
+      </div>
+      <ReactLenis
+        root
+        options={{
+          lerp: 0.05,
+        }}
+      >
+        <div className="form" ref={ref1}>
+          <motion.div
+            className="touches"
+            style={{ x: scrollITouch, opacity: scrollOpacity }}
+          >
+            <h2>GET IN TOUCH</h2>
+            <div className="touch">
+              <h4>Address:</h4>
+              <p>-------</p>
+            </div>
+            <div className="touch">
+              <h4>Phone:</h4>
+              <p>+-- ------</p>
+            </div>
+            <div className="touch">
+              <h4>Fax:</h4>
+              <p>-------</p>
+            </div>
+            <div className="touch">
+              <h4>Email:</h4>
+              <p>----@---.com</p>
+            </div>
+          </motion.div>
+          <motion.div
+            className="send"
+            style={{ x: scrollSend, opacity: scrollOpacity }}
+          >
+            <form ref={form} onSubmit={sendEmail}>
+              <div className="info">
+                <label htmlFor="name">First Name:</label>
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  value={user.name}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="info">
+                <label htmlFor="email">Email:</label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  value={user.email}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="info">
+                <label htmlFor="comment">Comment:</label>
+                <textarea
+                  id="comment"
+                  name="comment"
+                  style={{ height: "20rem" }}
+                  value={user.comment}
+                  onChange={handleChange}
+                />
+              </div>
+              <button type="submit" className="room-btn">
+                Submit
+              </button>
+            </form>
+            {formStatus && (
+              <div className="formStatusMessage" style={{ marginTop: "1rem" }}>
+                {formStatus}
+              </div>
+            )}
+          </motion.div>
+        </div>
+      </ReactLenis>
+    </div>
+  );
+};
+
+export default ContactUs;
